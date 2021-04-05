9f7a7bbe5bd991fdd6544d7f6ad4d679c0680ccb
Rio <rjauregu@stedwards.edu>
2021-04-05 16:53:04 -0500

    created an R script to contain manuscript 1 theme. Removed the fig_one_theme from the trafficking_plot_and_dunn_test files since it is now contained in it's own script

---
+++ scripts/fig_one_theme.R
@@ -0,0 +1,15 @@
+#jitter and theme elements to feed into ggplots
+fig_one_theme <- theme(axis.text.x = element_markdown(face = "italic", size = 13),
+                       axis.title.x = element_blank(),
+                       axis.ticks.x = element_blank(),
+                       panel.background = element_blank(),
+                       panel.grid.major.x = element_line(color = "snow3"),
+                       panel.grid = element_line(linetype = "solid", color = "snow3"),
+                       panel.border = element_rect(linetype = "solid", fill = NA),
+                       text = element_text(size = 24),
+) #this will need to be resized later, maybe like to 18 or so
+
+#position jitter function
+posn_j <- position_jitter(width=.2)
+
+```
